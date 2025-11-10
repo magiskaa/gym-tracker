a7e8072ebb4dc5f9320c794e71e4bd30302ead98
Valtteri Antikainen <vantikaine@gmail.com>
2025-11-10 22:53:20 +0200

    Initialized project with package.json and dependencies, and created a snub express server

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# gym-tracker
+# Gym Tracker
+
+
+
+![Node.js](https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white)
+![Express.js](https://img.shields.io/badge/Express.js-404D59?style=for-the-badge&logo=express&logoColor=white)
+![HTML5](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white)
+![CSS3](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css&logoColor=white)
+![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black)
---
+++ backend/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const PORT = 4000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "..", "frontend", "views"));
+app.use(express.static(path.join(__dirname, "..", "frontend", "public")));
+app.use(express.urlencoded({ extended: true }));
+
+const workoutsPath = path.join(__dirname,"..", "data", "workouts.json");
+const exercisesPath = path.join(__dirname,"..", "data", "exercises.json");
+
+let presetWorkouts = [];
+let completedWorkouts = [];
+let exercises = [];
+
+function loadData() {
+    try {
+        const workoutData = fs.readFileSync(workoutsPath, "utf8");
+        const workoutDataParsed = JSON.parse(workoutData);
+        presetWorkouts = workoutDataParsed.preset_workouts || [];
+        completedWorkouts = workoutDataParsed.completedWorkouts || [];
+
+        const exerciseData = fs.readFileSync(exercisesPath, "utf8");
+        const exercisesDataParsed = JSON.parse(exerciseData);
+        exercises = exercisesDataParsed.exercises || [];
+    } catch (e) {
+        console.error("Error loading data: ", e);
+    }
+}
+loadData();
+
+app.get("/", (req, res) => {
+    res.render("workouts", { presetWorkouts: presetWorkouts, completedWorkouts: completedWorkouts, exercises: exercises });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
